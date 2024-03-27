bdf2dfd8f8b51b6727d2f88a5b103f6222ca3298
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2024-03-27 23:30:27 +0530

    Project created

---
+++ src/components/CategoryMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CategoryMenu = () => {
+  return (
+    <div>CategoryMenu</div>
+  )
+}
+
+export default CategoryMenu
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import CategoryMenu from '../components/CategoryMenu'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar/>
+    <CategoryMenu/>
+    </>
+  )
+}
+
+export default Home
