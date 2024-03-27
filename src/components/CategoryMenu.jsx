09caf935146bf3277d369e6aba9638fb4677d597
Atanu Chakraborty <127716168+98atanu@users.noreply.github.com>
2024-03-28 00:28:40 +0530

    items fixed

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const CategoryMenu = () => {
   return (
-    <div>CategoryMenu</div>
+    <div className='mx-6'>
+        <h3 className='text-xl font-semibold'>Find the best food</h3>
+        <div className='my-5 flex gap-3 overflow-x-scroll scroll-smooth md:overflow-x-hidden'>
+            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>All</button>
+            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Lunch</button>
+            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Breakfast</button>
+            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Dinner</button>
+            <button className='px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-green-500 hover:text-white'>Snacks</button>
+        </div>
+    </div>
   )
 }
 
